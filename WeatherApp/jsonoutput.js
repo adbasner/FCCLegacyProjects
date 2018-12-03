9b077a0cf801ea5bd33db06cd11825c5415ef588
Andrew Basner <andrewbasner@gmail.com>
2018-12-03 04:37:32 -0600

    add quote and weather tracker

---
+++ WeatherApp/jsonoutput.js
@@ -0,0 +1,47 @@
+{
+  "coord":
+    {
+      "lon":-86.18,
+      "lat":41.67
+    },
+  "weather":[
+              {
+                "id":800,
+                "main":"Clear",
+                "description":"clear sky",
+                "icon":"https://cdn.glitch.com/6e8889e5-7a72-48f0-a061-863548450de5%2F01d.png?1499366022009"
+              }
+            ],
+  "base":"stations",
+  "main":
+    {
+      "temp":30.71,
+      "pressure":1014,
+      "humidity":48,
+      "temp_min":30,
+      "temp_max":31
+    },
+  "visibility":16093,
+  "wind":
+    {
+      "speed":4.6,
+      "deg":150
+    },
+  "clouds":
+    {
+      "all":1
+    },
+  "dt":1502921760,
+  "sys":
+    {
+      "type":1,
+      "id":1050,
+      "message":0.0049,
+      "country":"US",
+      "sunrise":1502880930,
+      "sunset":1502930471
+    },
+  "id":4923670,
+  "name":"Mishawaka",
+  "cod":200
+}
